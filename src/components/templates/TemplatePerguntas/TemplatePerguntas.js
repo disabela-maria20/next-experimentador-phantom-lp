030c92e4c280748dddff7a8f2ce17556b3d9ab24
Leonardo Simões <leooz.simoes@live.com>
2021-09-06 19:12:58 -0300

    fix: :bug: fix quot bug

--- src/components/templates/TemplatePerguntas/TemplatePerguntas.js
+++ src/components/templates/TemplatePerguntas/TemplatePerguntas.js
@@ -2,6 +2,7 @@ import Collapsible from "react-collapsible"
 import Title from "../../atom/Title/Title"
 import ContainerParagraph from "../../organism/ContainerParagraph/ContainerParagraph"
 import HeaderGeral from "../../organism/HeaderGeral/HeaderGeral"
+import Link from 'next/link'
 
 
 function TemplatePerguntas() {
@@ -16,12 +17,12 @@ function TemplatePerguntas() {
                 </Collapsible>
                 <Collapsible trigger="Como pedir amostra no Experimentador?" className="border-b border-grey-200 py-18" openedClassName="border-b border-purple-200 py-18" triggerClassName="py-18 md:text-26 tm:text-18 font-bold text-black w-full" triggerOpenedClassName="py-18 md:text-26 tm:text-18 font-bold text-purple w-full" >
                     <p className="py-18">
-                        1. Em seu smartphone ou smart speaker, diga o comando: "Ok Google, falar com Experimentador" ou "Alexa, abrir Experimentador" 2. Siga os passos de acordo com a experiência 3. Você receberá um sms que funciona como um token único e intransferível, clique e será direcionado para o último passo. 4. Finalize o formulário com seus dados, que devem estar atualizados. Dentro do prazo, receberá sua amostra.
+                        1. Em seu smartphone ou smart speaker, diga o comando: &quot;Ok Google, falar com Experimentador&quot; ou &quot;Alexa, abrir Experimentador&quot; 2. Siga os passos de acordo com a experiência 3. Você receberá um sms que funciona como um token único e intransferível, clique e será direcionado para o último passo. 4. Finalize o formulário com seus dados, que devem estar atualizados. Dentro do prazo, receberá sua amostra.
                     </p>
                 </Collapsible>
                 <Collapsible trigger="O experimentador é confiável?" className="border-b border-grey-200 py-18" openedClassName="border-b border-purple-200 py-18" triggerClassName="py-18 md:text-26 tm:text-18 font-bold text-black w-full" triggerOpenedClassName="py-18 md:text-26 tm:text-18 font-bold text-purple w-full" >
                     <p className="py-18"> 
-                        Sim. O Experimentador é um serviço criado pela Adventures.Inc, sediada em São Paulo - SP sobre o CNPJ: 30.798.660/0001-20 e tem como objetivo oferecer uma experiência digital de distribuição de amostras de produtos entre marcas e seus consumidores.
+                        Sim. O Experimentador é um serviço criado pela <Link href="https://adventures.inc"><a className="hover:undeline" target="_blank">Adventures.Inc</a></Link>, sediada em São Paulo - SP sobre o CNPJ: 30.798.660/0001-20 e tem como objetivo oferecer uma experiência digital de distribuição de amostras de produtos entre marcas e seus consumidores.
                     </p>
                 </Collapsible>
                 <Collapsible trigger="Quanto tempo demora para receber uma amostra no Experimentador?" className="border-b border-grey-200 py-18" openedClassName="border-b border-purple-200 py-18" triggerClassName="py-18 md:text-26 tm:text-18 font-bold text-black w-full" triggerOpenedClassName="py-18 md:text-26 tm:text-18 font-bold text-purple w-full" >
