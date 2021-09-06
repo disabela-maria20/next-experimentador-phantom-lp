b373aba7c6de79cbc4c4d1ec6457bef191aed78f
Leonardo Simões <leooz.simoes@live.com>
2021-09-06 12:29:35 -0300

    fix: :bug: fix gtm code

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -30,7 +30,7 @@ class Documentos extends Document {
                     }return n._iq[e]};e.amplitude=n})(window,document);
 
                     amplitude.getInstance().init("72d4f18dbb445c3109592dfe63c7d4de");` }} />
-                    <script dangerouslySetInnerHTML={{_html: `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    <script dangerouslySetInnerHTML={{__html: `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
