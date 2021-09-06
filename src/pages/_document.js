f6a71c1c321e55de329b55de659fc2ac5164529d
Leonardo Simões <leooz.simoes@live.com>
2021-09-06 14:21:23 -0300

    fix: :bug: Fix gtm

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,10 +5,19 @@ class Documentos extends Document {
         return(
             <Html lang="pt_BR">
                 <Head>
+                <script dangerouslySetInnerHTML={{
+                    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-KCF5KJP');`,
+                }}>
+  </script>
                 </Head>
                 <body>
                     <Main />
                     <NextScript />
+                    TagManager.initialize()
                 </body>
             </Html>
         )
