bed9ef3c86028bab2522fbf640b65f83ba375574
Leonardo Simões <68609532+leoozsimoes@users.noreply.github.com>
2021-09-28 10:46:04 -0300

    Update link navbar

--- src/components/molecule/Navbar/Navbar.js
+++ src/components/molecule/Navbar/Navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
     return (
         <nav className="tm:py-15 md:py-22 tm:h-79 md:h-95 bg-white shadow-xl">
             <div className="tm:container mx-auto grid grid-cols-1 items-center content-center justify-center">
-                <div className="flex justify-center"><Link href="/pacorabanne"><a><img src="/img/webp/logo-phantom.webp" alt="Phantom by Paco Rabane" width="200px" /></a></Link></div>
+                <div className="flex justify-center"><Link href="https://experimentador.com.br/pacorabanne"><a><img src="/img/webp/logo-phantom.webp" alt="Phantom by Paco Rabane" width="200px" /></a></Link></div>
             </div>
         </nav>
     )
