5e26c00acef47413ffef81d009400cb0fbef5046
Leonardo Simões <leooz.simoes@live.com>
2021-10-07 10:39:49 -0300

    feat: :sparkles: Adicionar email

--- src/components/organism/Footer/Footer.js
+++ src/components/organism/Footer/Footer.js
@@ -17,7 +17,9 @@ function Footer() {
                 </div>
                 <div className="grid grid-cols-1 pt-38">
                     <p className="text-13 text-white text-center"><Link href="/pacorabanne/politica-de-privacidade"><a className="hover:underline hover:text-purple">Política de Privacidade</a></Link></p>
-                    <p className="text-13 text-white text-center"><Link href="/pacorabanne/perguntas-frequentes"><a className="hover:underline hover:text-purple">Perguntas Frequentes</a></Link></p> <br /><span className="text-13 text-white text-center">Todos direitos reservados a paco rabanne © em parceira com experimentador</span>
+                    <p className="text-13 text-white text-center"><Link href="/pacorabanne/perguntas-frequentes"><a className="hover:underline hover:text-purple">Perguntas Frequentes</a></Link></p> <br />
+                    <p className="text-13 text-white text-center"><Link href="mailto:sac@experimentador.com.br"><a className="hover:underline hover:text-purple">sac@experimentador.com.br</a></Link></p>
+                    <span className="text-13 text-white text-center">Todos direitos reservados a paco rabanne © em parceira com experimentador</span>
                 </div>
             </div>
         </footer>
--- src/components/templates/TemplateFinalizado/TemplateFinalizado.js
+++ src/components/templates/TemplateFinalizado/TemplateFinalizado.js
@@ -3,6 +3,7 @@ import Title from "../../atom/Title/Title"
 import Navbar from "../../molecule/Navbar/Navbar"
 import Conversao from "../../organism/Conversao/Conversao"
 import Footer from "../../organism/Footer/Footer"
+import Link from "next/link"
 
 function TemplateFinalizado() {
     return (
@@ -17,6 +18,7 @@ function TemplateFinalizado() {
                         <Title titulo="Poxa, acabaram as amostras :(" extraclass="text-white"/>
                         <Paragraph paragrafo="Mas temos uma surpresa para você. Na compra do perfume Phantom, nos tamanhos de 50 ou 100 ml na Beleza na Web, você ganhará uma miniatura do perfume. Informe seu e-mail e desbloqueie seu cupom." extraclass="text-white py-38" />
                         <Conversao />
+                        <span className="text-13 pt-10 ">Em caso de dúvidas: <Link href="mailto:sac@experimentador.com.br"><a className="hover:underline">sac@experimentador.com.br</a></Link></span>
                     </div>
                 </div>
             </section>
